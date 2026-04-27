3f6bf40c85d9c894619e48dee44b6f229a527ea5
Timothy <hy.timothy@gmail.com>
2026-04-27 08:29:14 +0700

    Update soal1.c

--- soal1.c
+++ soal1.c
@@ -1,5 +1,76 @@
+/** EL2008 Praktikum Pemecahan Masalah dengan Pemrograman 2024/2025
+ *   Modul               : 2
+ *   Hari dan Tanggal    : Selasa, 21 April 2026
+ *   Nama (NIM)          : 13224024
+ *   Nama File           : soal1.c
+ *   Deskripsi           : 
+ * 
+ */
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(void) {
+int jumlah = 0;
+int CAP = 100;
+
+
+int *add (int * data, int elemen){
+    if(jumlah >= CAP-2){
+        CAP = CAP + 50;
+        int *temp = (int *)realloc(data, CAP*sizeof(int)); 
+        if (temp == NULL) {
+            printf("Memory expansion failed!\n");
+            CAP = CAP - 50; 
+            return data;    
+        }
+        data = temp;
+    } 
+    data[jumlah] = elemen;
+    jumlah ++;
+    return data;
+}
+
+void sort (int *data){
+    int temp;
+    for (int i = 0; i < jumlah-1; i++)
+    {
+        for (int j = 0; j < jumlah-1-i; j++)
+        {
+            if (data[j] > data[j+1])
+            {
+                temp = data[j+1];
+                data[j+1] = data[j];
+                data[j] = temp;
+            }
+        }
+    }    
+}
+
+int main(){
+    
+    int *data = (int *)malloc(CAP * sizeof(int));
+    int input;
+    do
+    {
+        scanf("%d", &input);
+        data = add(data, input);
+
+    } while (input != -1);
+    jumlah --;
+
+
+    printf("COUNT %d\n", jumlah);
+    sort(data); printf("SORTED ");
+    for (int i = 0; i < jumlah; i++)
+    {
+        printf("%d ", data[i]);
+    }
+    printf("\n");
+    if (jumlah%2 == 0)
+    {
+        printf("MEDIAN %d", 0.5*(data[jumlah/2] + data[jumlah/2 + 1]));
+    }
+    else{
+        printf("MEDIAN %d", data[jumlah/2]);
+    }
     return 0;
 }
